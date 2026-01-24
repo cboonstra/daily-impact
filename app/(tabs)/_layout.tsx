fc22f42cc7f78cfc94addfabaf5ea388ed9ee35b
cboonstra <lotte.boonstra@asrr.nl>
2026-01-24 20:00:19 +0100

    adjust toolbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,8 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'SDGs',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="globe.europe.africa.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="daily"
+        options={{
+          title: 'Daily',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="checkmark.circle.fill" color={color} />,
         }}
       />
       <Tabs.Screen
@@ -28,6 +35,13 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
     </Tabs>
